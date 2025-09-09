87af627ac2c12a3072bbbe1e3f53588782872263
Kyoungsu Kim <kyoungsukim@Kyoungsuui-MacBookAir.local>
2025-09-09 15:37:21 +0900

    feat: init

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import RevenueDistributionChart from "../src/components/charts/revenue-distribution-chart"
-
-export default function SyntheticV0PageForDeployment() {
-  return <RevenueDistributionChart />
-}
